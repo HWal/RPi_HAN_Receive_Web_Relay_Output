78b6bac1bf93486d37147fe997cf01a24ce543ce
HWal <helgewal@gmail.com>
2024-09-08 22:08:11 +0200

    Edit files

--- Python_AMS/maxpowermonitor.py
+++ Python_AMS/maxpowermonitor.py
@@ -13,8 +13,8 @@ from email.message import EmailMessage
 
 # Initialization of variables
 from_email_addr ="helge5895@gmail.com"
-from_email_pass ="plxwxihtiaddnaum"
-to_email_addr ="helgewal@gmail.com"
+from_email_pass ="xxxxxxxxxxxxxxxx"
+to_email_addr ="yyyyyyyyyy@zzzzzzz"
 
 ok1 = False
 ok2 = False
